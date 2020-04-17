27443a805f4f25e35a573fe1dece15ead77c71b8
Like Sha <tradersalike88@yahoo.com>
2020-04-17 14:52:17 -0700

    updated Driver utilities

--- src/test/java/com/weborders/utilities/Driver.java
+++ src/test/java/com/weborders/utilities/Driver.java
@@ -17,9 +17,13 @@ public class Driver {
 
     }
 
+    /**synchronized makes method thread safe. It ensures that only 1 thread can use it at the time.
+     *
+     * Thread safety reduces performance but it makes everything safe.
+     *
+     * @return
+     */
     public synchronized static WebDriver getDriver() {
-        //synchronized makes method thread safe,it ensures that only 1 thread can use it at the time
-        //thread safety reduces performance but it makes everything safe
         //if webdriver object doesn't exist
         //create it
         if (driverPool.get() == null) {
@@ -50,10 +54,46 @@ public class Driver {
         return driverPool.get();
     }
 
+    /**synchronized makes method thread safe. It ensures that only 1 thread can use it at the time.
+     *
+     * Thread safety reduces performance but it makes everything safe.
+     *
+     * @return
+     */
+    public synchronized static WebDriver getDriver(String browser) {
+        //if webdriver object doesn't exist
+        //create it
+        if (driverPool.get() == null) {
+            //specify browser type in configuration.properties file
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().version("79").setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--start-maximized");
+                    driverPool.set(new ChromeDriver(chromeOptions));
+                    break;
+                case "chromeheadless":
+                    //to run chrome without interface (headless mode)
+                    WebDriverManager.chromedriver().version("79").setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.setHeadless(true);
+                    driverPool.set(new ChromeDriver(options));
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver());
+                    break;
+                default:
+                    throw new RuntimeException("Wrong browser name!");
+            }
+        }
+        return driverPool.get();
+    }
+
     public static void closeDriver() {
         if (driverPool != null) {
             driverPool.get().quit();
-            driverPool = null;
+            driverPool.remove();
         }
     }
 }
