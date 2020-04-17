79d53f09c3e5f62cb84053c40aa30f3b8ed8ba58
Like Sha <tradersalike88@yahoo.com>
2020-04-17 14:59:55 -0700

    updated AbstractBasePage

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -10,13 +10,12 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-/*
-It meant to be extended
+/**
+ * It meant to be extended
  */
 public abstract class AbstractBasePage {
-
     protected WebDriver driver = Driver.getDriver();
-    protected WebDriverWait wait = new WebDriverWait(driver,20);
+    protected WebDriverWait wait = new WebDriverWait(driver, 20);
 
     @FindBy(tagName = "h1")
     protected WebElement pageLogo;
@@ -29,21 +28,20 @@ public abstract class AbstractBasePage {
         return pageSubtitle.getText().trim();
     }
 
-    public String getPageLogoText(){
+    public String getPageLogoText() {
         return pageLogo.getText();
     }
 
-    public AbstractBasePage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public AbstractBasePage() {
+        PageFactory.initElements(driver, this);
     }
 
     /**
-     * Specify component name as a parameter, like : View all products or Orders
+     * Specify component name as a parameter, like: View all products or Orders
      * @param component
      */
-    public void navigateTo(String component){
-        String locator = "//a[text()='"+component+"']";
+    public void navigateTo(String component) {
+        String locator = "//a[text()='" + component + "']";
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
     }
-
 }
