a9259166bc3817b15efbfff2b7687286acc0c371
Like Sha <tradersalike88@yahoo.com>
2020-04-17 14:53:58 -0700

    updated AbstractBaseTest

--- src/test/java/com/weborders/tests/AbstractBaseTest.java
+++ src/test/java/com/weborders/tests/AbstractBaseTest.java
@@ -1,5 +1,6 @@
 package com.weborders.tests;
 
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
@@ -12,19 +13,19 @@ import org.testng.annotations.*;
 
 import java.io.IOException;
 
+
 public abstract class AbstractBaseTest {
 
-    protected WebDriver driver ;
-
+    protected WebDriver driver;
     protected static ExtentReports extentReports;
     protected static ExtentHtmlReporter extentHtmlReporter;
     protected static ExtentTest extentTest;
 
-
     @BeforeTest
-    public void beforeTest(){
+    public void beforeTest() {
         extentReports = new ExtentReports();
         String reportPath = "";
+
         if (System.getProperty("os.name").toLowerCase().contains("win")) {
             reportPath = System.getProperty("user.dir") + "\\test-output\\report.html";
         } else {
@@ -37,41 +38,37 @@ public abstract class AbstractBaseTest {
     }
 
     @AfterTest
-    public void afterTest(){
+    public void afterTest() {
         extentReports.flush();
     }
 
     @BeforeMethod
     @Parameters("browser")
-    public void setup(@Optional String browser){
-        System.out.println("Browser type : " +browser);
-        driver = Driver.getDriver();
+    public void setup(@Optional String browser) {
+        System.out.println("Browser type: " + browser);
+        driver = browser == null ? Driver.getDriver() : Driver.getDriver(browser);
         driver.get(ConfigurationReader.getProperty("url"));
         driver.manage().window().maximize();
     }
 
     @AfterMethod
-    public void tearDown(ITestResult testResult) {
-        if(testResult.getStatus()==ITestResult.FAILURE){
+    public void teardown(ITestResult testResult) {
+        if (testResult.getStatus() == ITestResult.FAILURE) {
             String screenshotLocation = BrowserUtilities.getScreenshot(testResult.getName());
             try {
-                extentTest.fail(testResult.getName());//TEST NAME THAT FAILED
+                extentTest.fail(testResult.getName());//test name that failed
                 extentTest.addScreenCaptureFromPath(screenshotLocation);//screenshot as an evidence
                 extentTest.fail(testResult.getThrowable());//error message
             } catch (IOException e) {
                 e.printStackTrace();
                 throw new RuntimeException("Failed to attach screenshot");
             }
-        }else if(testResult.getStatus()==ITestResult.SUCCESS){
+        } else if (testResult.getStatus() == ITestResult.SUCCESS) {
             extentTest.pass(testResult.getName());
-        }else if(testResult.getStatus()==ITestResult.SKIP){
+        } else if (testResult.getStatus() == ITestResult.SKIP) {
             extentTest.skip(testResult.getName());
         }
         BrowserUtilities.wait(3);
         Driver.closeDriver();
     }
-
-
-
-
 }
