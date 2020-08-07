d5a60a531714195e8b35abf2bab4fa9f64d8767b
Like Sha <tradersalike88@yahoo.com>
2020-08-06 18:00:30 -0700

    update some notes on ViewAllOrderPage

--- src/test/java/com/weborders/pages/ViewAllOrdersPage.java
+++ src/test/java/com/weborders/pages/ViewAllOrdersPage.java
@@ -1,4 +1,20 @@
 package com.weborders.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 public class ViewAllOrdersPage  extends AbstractBasePage{
+    /*
+    //a[text()='View all orders']
+
+    see AbstractBasePage class for these 3 pages
+
+    /**
+     * Specify component name as a parameter, like: View all products or Orders
+     * @param component
+     */
+//    public void navigateTo(String component) {
+//        String locator = "//a[text()='" + component + "']";
+//        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
+//     */
 }
